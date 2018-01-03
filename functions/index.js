f57907492ed7dc8813cdd7c31da683c3738af92e
Patrick Walker <bpatrick.walker@gmail.com>
2018-01-03 13:45:11 +0000

    New function to add download URL to realtime DB

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ const functions = require('firebase-functions');
 // });
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
+const gcstore = require('@google-cloud/storage')()
 const ref = admin.database().ref()
 
 exports.createUserAccount = functions.auth.user().onCreate(event => {
@@ -26,4 +27,35 @@ exports.cleanupUser = functions.auth.user().onDelete(event => {
     return newUserRef.update({
         deleted: true
     })
+})
+
+exports.syncDBStorage = functions.storage.object.onChange(event => {
+    const object = event.data
+    const filePath = object.name; // File path in the bucket.
+    const resourceState = object.resourceState; // The resourceState is 'exists' or 'not_exists' (for file/folder deletions).
+    const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+    const metaData = object.metadata
+    // Exit if this is a move or deletion event.
+    if (resourceState === 'not_exists') {
+    console.log('This is a deletion event.');
+    return;
+    }
+
+    // Exit if file exists but is not new and is only being triggered
+    // because of a metadata change.
+    if (resourceState === 'exists' && metageneration > 1) {
+    console.log('This is a metadata change event.');
+    return;
+    }
+    //Ok at this point we know it's a creation event and we have metadata so we should pull our custom ID 
+    var ImageID = metaData['customMetadata']['ID']
+    //We also really want the download URL as well so we should create a storage ref and grab it
+    var downUrl = storage.ref(filePath).getDownloadURL()
+    
+    const imageRef = ref.child('/goats/${ImageID}')
+    return imageRef.update({
+        downloadUrl: downUrl,
+        filePath: filePath
+    })
+    
 })
