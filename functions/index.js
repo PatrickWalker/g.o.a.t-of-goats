3af4678ce1c76dd30bf51a7a5b806d901a3171e9
Patrick Walker <bpatrick.walker@gmail.com>
2018-01-03 10:57:59 +0000

    First Firebase Function to add a users section to DB when someone is created and set them as soft deleted when they leave

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,24 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+const ref = admin.database().ref()
+
+exports.createUserAccount = functions.auth.user().onCreate(event => {
+    const uid = event.data.uid
+    const email = event.data.email
+    const newUserRef = ref.child('/users/${uid}')
+    return newUserRef.set({
+        email: email
+    })
+})
+
+exports.cleanupUser = functions.auth.user().onDelete(event => {
+    const uid = event.data.uid
+    const email = event.data.email
+    const UserRef = ref.child('/users/${uid}')
+    return newUserRef.update({
+        deleted: true
+    })
+})
